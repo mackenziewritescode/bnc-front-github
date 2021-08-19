f5517dc2891675fac0a0b89cef515148a6e56dce
Mackenzie CHARLTON <m.charlton@groupeonepoint.com>
2021-08-19 11:17:52 -0400

    added monorepo

---
+++ packages/common/dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __importStar(require("react"));
+var react_native_1 = require("react-native");
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ccc",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
+function App() {
+    return (React.createElement(react_native_1.View, { style: styles.container },
+        React.createElement(react_native_1.Text, null, "React Native App")));
+}
+exports.default = App;
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,3 @@
+import App from "@bnc/common";
+
+export default App;
