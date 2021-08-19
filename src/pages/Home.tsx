ed537222151fae914041a38a76ea4dc30e144ff6
Mackenzie CHARLTON <m.charlton@groupeonepoint.com>
2021-08-19 10:28:46 -0400

    updated configure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,36 +4,32 @@ import { Link } from "react-router-dom";
 
 export type Props = {
   name: string;
-  baseEnthusiasmLevel?: number;
+  defaultCount?: number;
 };
 
-const Home: React.FC<Props> = ({ name = "", baseEnthusiasmLevel = 0 }) => {
-  const [enthusiasmLevel, setEnthusiasmLevel] =
-    React.useState(baseEnthusiasmLevel);
+const Home: React.FC<Props> = ({ name = "User", defaultCount = 0 }) => {
+  const [count, setCount] = React.useState(defaultCount);
 
-  const onIncrement = () => setEnthusiasmLevel(enthusiasmLevel + 1);
-  const onDecrement = () =>
-    setEnthusiasmLevel(enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0);
-
-  const getExclamationMarks = (numChars: number) =>
-    numChars > 0 ? Array(numChars + 1).join("!") : "";
+  const onIncrement = () => setCount(count + 1);
+  const onDecrement = () => setCount(count > 0 ? count - 1 : 0);
 
   return (
     <View style={styles.container}>
       <Link to="./configure">Configure</Link>
       <Text style={styles.greeting}>
-        Hello {name}
-        {getExclamationMarks(enthusiasmLevel)}
+        Hello {name}.
+        <br />
+        Count: {count}
       </Text>
       <View>
         <Button
-          title="Increase enthusiasm"
+          title="Increase count"
           accessibilityLabel="increment"
           onPress={onIncrement}
           color="blue"
         />
         <Button
-          title="Decrease enthusiasm"
+          title="Decrease count"
           accessibilityLabel="decrement"
           onPress={onDecrement}
           color="red"
