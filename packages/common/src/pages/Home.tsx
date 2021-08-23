ba43fe7d7ec8bc9c6b4823ec3628e99a2f2e704a
Mackenzie CHARLTON <m.charlton@groupeonepoint.com>
2021-08-23 10:45:48 -0400

    trying to add teamsfx. need to add .env

--- packages/common/src/pages/Home.tsx
+++ packages/common/src/pages/Home.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { Link } from "react-router-dom";
+import * as microsoftTeams from "@microsoft/teams-js";
+import { TeamsUserCredential } from "@microsoft/teamsfx";
 
 export type Props = {
   name: string;
   defaultCount?: number;
 };
 
-const Home: React.FC<Props> = ({ name = "User", defaultCount = 0 }) => {
+const Home: React.FC<Props> = ({ name, defaultCount = 0 }) => {
   const [count, setCount] = React.useState(defaultCount);
 
   const onIncrement = () => setCount(count + 1);
   const onDecrement = () => setCount(count > 0 ? count - 1 : 0);
+
+  const { isInTeams } = useTeamsFx();
+
+  const userProfile = useData(async () => {
+    const credential = new TeamsUserCredential();
+    return isInTeams ? await credential.getUserInfo() : undefined;
+  })?.data;
+  name = userProfile ? userProfile.displayName : "User";
 
   return (
     <View style={styles.container}>
